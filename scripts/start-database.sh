50ddd9d5b496bb60a5f954ac4c87c192f3f68510
Hatem <hatem.hm.7@gmail.com>
2020-03-10 17:50:34 +0200

    Fix database script - adding mapped port

--- scripts/start-database.sh
+++ scripts/start-database.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --name app-db -e MYSQL_DATABASE=app-db -e MYSQL_ROOT_PASSWORD=secret -d mysql
+docker run --name app-db -p 3306:3306  -e MYSQL_DATABASE=app-db -e MYSQL_ROOT_PASSWORD=secret -d mysql
